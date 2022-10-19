b843ea02a0eaf5e796b38ff06845da18c853300e
Daniel Silva <dfsilva@iMac-de-Daniel.local>
2022-10-19 15:41:05 -0300

    feat: edit database

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "wongames"),
-      user: env("DATABASE_USERNAME", "wongames"),
+      user: env("DATABASE_USERNAME", "dfsilva"),
       password: env("DATABASE_PASSWORD", "#.su2023.#"),
       ssl: env.bool("DATABASE_SSL", false),
     },
